500c7eed353c5349b6dd0a5c00c6b4be36414fd5
PHILIPAKIS Thomas (EXT SYNAPTIX) <PTPS12161@COMMUN.AD.SNCF.FR>
2016-09-09 20:21:20 +0200

    fix for possible awt thread lock when button click disposes a window containing the button

--- src/main/java/com/synaptix/toast/adapter/swing/handler/button/JButtonActionHandler.java
+++ src/main/java/com/synaptix/toast/adapter/swing/handler/button/JButtonActionHandler.java
@@ -3,6 +3,7 @@ package com.synaptix.toast.adapter.swing.handler.button;
 import java.util.concurrent.CountDownLatch;
 
 import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,20 +23,35 @@ public class JButtonActionHandler implements
 	public String handle(final JButton button, CommandRequest command) {
 		switch (command.action) {
 		case CLICK:
-			final CountDownLatch latch = new CountDownLatch(1);
-			FestRobotInstance.runOutsideEDT(new Runnable() {
-				@Override
-				public void run() {
-					button.doClick();
-					latch.countDown();
+			//FIX for possible lock on AWT thread when the button click disposes the window containing the button
+			// attention FestRobotInstance must not be started on EDT
+			// the problem is the deadlock when the button click disposes 
+			// the window containing the button in a certain way
+			// happens with some SNCF dialog windows
+			if(SwingUtilities.isEventDispatchThread()){
+				try{
+					FestRobotInstance.getRobot().click(button);
+					return ResultKind.SUCCESS.name();
+				} catch(Exception e){
+					LOG.warn(e.getMessage(), e);
+					return ResultKind.SUCCESS.name();
 				}
-			});
-			try {
-				latch.await();
-				return ResultKind.SUCCESS.name();
-			} catch (InterruptedException e) {
-				LOG.error(e.getMessage(), e);
-				return ResultKind.ERROR.name();
+			} else {
+				final CountDownLatch latch = new CountDownLatch(1);
+				FestRobotInstance.runOutsideEDT(new Runnable() {
+					@Override
+					public void run() {
+						button.doClick();
+						latch.countDown();
+					}
+				});
+				try {
+					latch.await();
+					return ResultKind.SUCCESS.name();
+				} catch (InterruptedException e) {
+					LOG.error(e.getMessage(), e);
+					return ResultKind.ERROR.name();
+				}
 			}
 		case ACTIVE:
 			if(button.isEnabled()){
